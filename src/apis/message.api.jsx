fc30b80babd73e9c99c75715505635e031a73527
ncbach <nguyenchienbach123@gmail.com>
2026-05-17 13:29:54 +0700

    fix: remove duplicate message rest api

--- src/apis/message.api.jsx
+++ src/apis/message.api.jsx
@@ -3,25 +3,23 @@ import authorizedAxios from "../helpers/authorizedAxios.js";
 const URL = `${import.meta.env.VITE_HOST_URL}/api/v1`;
 
 const API_URL = {
-    GET_MESSAGES_BY_CONVERSATION: (conversationId) =>
-        `${URL}/messages/conversation/${conversationId}`,
+    GET_MESSAGES_BY_CONVERSATION: (conversationId, page, size) =>
+        `${URL}/messages/conversation/${conversationId}/paged?page=${page}&size=${size}`,
 
     MARK_CONVERSATION_READ: (conversationId) =>
         `${URL}/messages/conversation/${conversationId}/read-all`,
-
-    SEND_MESSAGE: `${URL}/messages`,
 };
 
 const MessageAPI = {
-    getMessagesByConversation: async (conversationId) => {
+    getMessagesByConversation: async (conversationId, page = 0, size = 50) => {
         try {
             const response = await authorizedAxios().get(
-                API_URL.GET_MESSAGES_BY_CONVERSATION(conversationId)
+                API_URL.GET_MESSAGES_BY_CONVERSATION(conversationId, page, size)
             );
 
             return {
                 isSuccess: true,
-                data: response.data.data || [],
+                data: response.data.data?.content || [],
                 message: response.data.message,
             };
         } catch (error) {
@@ -55,30 +53,6 @@ const MessageAPI = {
                 data: null,
                 message:
                     error.response?.data?.message || "Không đánh dấu đã đọc được",
-            };
-        }
-    },
-
-    sendTextMessageREST: async (conversationId, content) => {
-        try {
-            const response = await authorizedAxios().post(API_URL.SEND_MESSAGE, {
-                conversation_id: conversationId,
-                content,
-                type: "TEXT",
-            });
-
-            return {
-                isSuccess: true,
-                data: response.data.data,
-                message: response.data.message,
-            };
-        } catch (error) {
-            console.error("SEND MESSAGE REST ERROR:", error);
-
-            return {
-                isSuccess: false,
-                data: null,
-                message: error.response?.data?.message || "Gửi tin nhắn thất bại",
             };
         }
     },
